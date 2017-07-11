a88a25ac774342965058479bcb41ff14ea7f3f27
300927812 <300927812@PR-A315-24>
2017-07-11 15:41:45 -0400

    Add SuperHuman Sub cLass

---
+++ COMP123-S2017-Lesson9A/SuperHuman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+ * Name- Simar Gill
+ * Date- July11,2017
+ * Description-THis is the SuperHuman sub class
+ * derive from
+ * Version- 0.1 - Created the SuperHuman sub class
+ */
+namespace COMP123_S2017_Lesson9A
+{
+    /// <summary>
+    /// This is the SuperHuman Sub Class
+    /// It inherits from the Human Superclass.
+    /// </summary>
+    public class SuperHuman : Human
+    {
+        // PRIVATE FILEDS
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+        public SuperHuman(string name)
+            :base(name)
+        {
+
+        }
+        // PUBLIC METHODS
+    }
+}
